bfaca0a27b5eb2a6031e2da38138ac4052ea863c
Adrián Bailador Panero <adrianbailador@hotmail.com>
2022-05-27 00:16:58 +0100

    Static API Paths

---
+++ PeliculasWeb/Utilidades/CT.cs
@@ -0,0 +1,12 @@
+﻿namespace PeliculasWeb.Utilidades
+{
+    public static class CT
+    {
+        public static string UrlBaseApi = "https://localhost:44367/";
+        public static string RutaCategoriasApi = UrlBaseApi + "api/Categorias/";
+        public static string RutaPeliculasApi = UrlBaseApi + "api/Peliculas/";
+        public static string RutaUsuariosApi = UrlBaseApi + "api/Usuarios/";
+
+        ///Faltan otras rutas para buscar y filtrar peliculas por categorias
+    }
+}
